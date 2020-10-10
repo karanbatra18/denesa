e7db0a0554daea590e03273d05f424f53457c4ca
karanbatra <karanbatra18@gmail.com>
2020-10-10 17:44:44 +0530

    Updates new

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -105,12 +105,12 @@
 </div>
 <!-- /.row -->
 <div class="row">
-    <div class="col-lg-8">
+    <div class="col-lg-12">
         <div class="panel panel-default">
             <div class="panel-heading">
                 <i class="fa fa-bar-chart-o fa-fw"></i> Monthly Registered Doctors
                 <div class="pull-right">
-                    <div class="btn-group">
+                    {{--<div class="btn-group">
                         <button type="button" class="btn btn-default btn-xs dropdown-toggle"
                                 data-toggle="dropdown">
                             Actions
@@ -127,7 +127,7 @@
                             <li><a href="#">Separated link</a>
                             </li>
                         </ul>
-                    </div>
+                    </div>--}}
                 </div>
             </div>
             <!-- /.panel-heading -->
@@ -139,7 +139,7 @@
         <!-- /.panel -->
     </div>
     <!-- /.col-lg-8 -->
-    <div class="col-lg-4">
+   {{-- <div class="col-lg-4">
         <div class="panel panel-default">
             <div class="panel-heading">
                 <i class="fa fa-bell fa-fw"></i> Notifications Panel
@@ -199,7 +199,7 @@
             <!-- /.panel-body -->
         </div>
         <!-- /.panel -->
-    </div>
+    </div>--}}
     <!-- /.col-lg-4 -->
 </div>
 <!-- /.row -->
@@ -210,6 +210,68 @@
         <script src="{{ asset('admin-theme/startmin-master') }}/js/raphael.min.js"></script>
         <script src="{{ asset('admin-theme/startmin-master') }}/js/morris.min.js"></script>
         <script src="{{ asset('admin-theme/startmin-master') }}/js/morris-data.js"></script>
+    <script>
+        Morris.Area({
+            element: 'morris-area-chart',
+            data: [{
+                period: '2010 Q1',
+                iphone: 2666,
+                ipad: null,
+                itouch: 2647
+            }, {
+                period: '2010 Q2',
+                iphone: 2778,
+                ipad: 2294,
+                itouch: 2441
+            }, {
+                period: '2010 Q3',
+                iphone: 4912,
+                ipad: 1969,
+                itouch: 2501
+            }, {
+                period: '2010 Q4',
+                iphone: 3767,
+                ipad: 3597,
+                itouch: 5689
+            }, {
+                period: '2011 Q1',
+                iphone: 6810,
+                ipad: 1914,
+                itouch: 2293
+            }, {
+                period: '2011 Q2',
+                iphone: 5670,
+                ipad: 4293,
+                itouch: 1881
+            }, {
+                period: '2011 Q3',
+                iphone: 4820,
+                ipad: 3795,
+                itouch: 1588
+            }, {
+                period: '2011 Q4',
+                iphone: 15073,
+                ipad: 5967,
+                itouch: 5175
+            }, {
+                period: '2012 Q1',
+                iphone: 10687,
+                ipad: 4460,
+                itouch: 2028
+            }, {
+                period: '2012 Q2',
+                iphone: 8432,
+                ipad: 5713,
+                itouch: 1791
+            }],
+            xkey: 'period',
+            ykeys: ['iphone', 'ipad', 'itouch'],
+            labels: ['iPhone', 'iPad', 'iPod Touch'],
+            pointSize: 2,
+            hideHover: 'auto',
+            resize: true
+        });
+    </script>
 
 @endpush
 
